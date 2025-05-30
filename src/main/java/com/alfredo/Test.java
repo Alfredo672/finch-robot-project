dc4f58c1935438a7f75b84fb902cd8b193d8cd7f
Alfredo <aortiz105@mymail.lausd.net>
2025-05-30 09:30:27 -0700

    updated Test.java

---
+++ src/main/java/com/alfredo/Test.java
@@ -0,0 +1,15 @@
+package com.alfredo;
+
+import com.birdbrain.Finch;
+
+public class Test {
+    public static void main(String[] arg) {
+        Finch bird = new Finch();
+
+
+
+
+        bird.stop();
+        bird.disconnect();
+    }
+}
