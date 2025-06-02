947191570ba4d110f88d4a927575f53212ae1507
Alfredo <aortiz105@mymail.lausd.net>
2025-06-02 09:36:57 -0700

    updated

--- src/main/java/com/alfredo/Test.java
+++ src/main/java/com/alfredo/Test.java
@@ -6,6 +6,16 @@ public class Test {
     public static void main(String[] arg) {
         Finch bird = new Finch();
 
+        // Move forward for 1 second
+        bird.setMove("F", 30, 100);
+
+   // Turn right for 0.5 seconds
+
+
+   // Move backward for 1 second
+
+
+   // Turn left for 0.5 seconds
 
 
 
