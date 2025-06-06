9a984a64be5371c8d594edd4d10e93bc3923ce63
Alfredo <aortiz105@mymail.lausd.net>
2025-06-06 11:30:19 -0700

    test run

--- src/main/java/com/alfredo/Test.java
+++ src/main/java/com/alfredo/Test.java
@@ -2,24 +2,33 @@ package com.alfredo;
 
 import com.birdbrain.Finch;
 
-public class Test {
-    public static void main(String[] arg) {
-        Finch bird = new Finch();
-
-        // Move forward for 1 second
-        bird.setMove("F", 30, 100);
-
-   // Turn right for 0.5 seconds
-
-
-   // Move backward for 1 second
-
-
-   // Turn left for 0.5 seconds
+public static void main(String[] args) {
+     Finch myFinch = new Finch();
+// Starting movements 
+     myFinch.setMove("F", 16,50);
+     myFinch.setTurn("R", 90, 50);
+     myFinch.setMove("F", 65,50);
+// Middle part of the maze
+     myFinch.setTurn("L", 90, 50);
+     myFinch.setMove("F", 60, 30);
+     myFinch.setTurn("L", 90, 50);
+     myFinch.setMove("F", 68,50);
 
 
+//turning into the long stretch of the maze
+     myFinch.setTurn("R", 92, 50);
+     myFinch.setMove("F", 115, 50);
 
-        bird.stop();
-        bird.disconnect();
-    }
+      //final turns until the end
+     myFinch.setTurn("R", 90, 50);
+     myFinch.setMove("F", 20, 50);
+     myFinch.setTurn("L", 90, 50);
+     myFinch.setMove("F", 28, 50);
+     myFinch.setTurn("L", 90, 50);
+     myFinch.setMove("F", 25, 50);
+
+
+     myFinch.stopAll();
+     myFinch.disconnect();
+}
 }
