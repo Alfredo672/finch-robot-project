388fdf5fdf19709b391991a9a29b584ff3471267
Alfredo <aortiz105@mymail.lausd.net>
2025-06-06 12:55:21 -0700

    final

--- src/main/java/com/alfredo/Test.java
+++ src/main/java/com/alfredo/Test.java
@@ -1,25 +1,28 @@
 package com.alfredo;
 
 import com.birdbrain.Finch;
-
+public class Test {
 public static void main(String[] args) {
      Finch myFinch = new Finch();
-// Starting movements 
+
+
+//starting sound
+     myFinch.playNote(60,0.5);
+//Getting out of the starting position 
      myFinch.setMove("F", 16,50);
      myFinch.setTurn("R", 90, 50);
      myFinch.setMove("F", 65,50);
-// Middle part of the maze
+
+     myFinch.playNote(100,0.5);
+//mid section
      myFinch.setTurn("L", 90, 50);
      myFinch.setMove("F", 60, 30);
      myFinch.setTurn("L", 90, 50);
      myFinch.setMove("F", 68,50);
-
-
-//turning into the long stretch of the maze
+//long stretch of the maze
      myFinch.setTurn("R", 92, 50);
      myFinch.setMove("F", 115, 50);
-
-      //final turns until the end
+         //final turns until the end
      myFinch.setTurn("R", 90, 50);
      myFinch.setMove("F", 20, 50);
      myFinch.setTurn("L", 90, 50);
